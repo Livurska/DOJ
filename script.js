4e09e7ae34d393fa254e20cb305555ada2bcede1
livurska <merilehto.iivar@gmail.com>
2024-08-03 20:51:27 +0300

    Co-authored-by: Iisak.merilehto@gmail.com <Iisak.merilehto@gmail.com>

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ const left_arrow = document.getElementById('left_arrow');
 const shoes = document.getElementById('shoes');
 const pallo1 = document.getElementById('pallo1');
 const pallo2 = document.getElementById('pallo2');
-const pallo3 = document.getElementById('pallo3');
+const banner2 = document.getElementById("banner2");
+const banner3 = document.getElementById("banner3");
 
-
+function onWindowLoad() {
+    setBannerPage();
+}
 
 right_arrow.addEventListener('click', function() {
-    if (banner1Page < 3) {
+    if (banner1Page < 2) {
         banner1Page += 1;
         setBannerPage();
         console.log('Right Click: Current Page:', banner1Page);
@@ -30,22 +33,18 @@ function setBannerPage() {
     console.log('Setting Banner Page to:', banner1Page);
     switch (banner1Page) {
         case 1:
-            shoes.style.left = '36vw';
+            shoes.style.left = '36.5vw';
             pallo1.src = 'pallofull.png';
             pallo2.src = 'palloTyhja.png';
-            pallo3.src = 'palloTyhja.png';
+            banner3.style.opacity = 0;
+            banner2.style.opacity = 1;
             break;
         case 2:
-            shoes.style.left = '-39vw';
+            shoes.style.left = '-40.5vw';
             pallo1.src = 'palloTyhja.png';
             pallo2.src = 'pallofull.png';
-            pallo3.src = 'palloTyhja.png';
-            break;
-        case 3:
-            shoes.style.left = '-118vw';
-            pallo1.src = 'palloTyhja.png';
-            pallo2.src = 'palloTyhja.png';
-            pallo3.src = 'pallofull.png';
+            banner2.style.opacity = 0;
+            banner3.style.opacity = 1;
             break;
         default:
             console.log('Invalid Page Number');
