f5b52814dbe7da15fda2d202a0052239f3afdc8b
livurska <merilehto.iivar@gmail.com>
2024-08-04 21:45:07 +0300

    Update 1

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const pallo1 = document.getElementById('pallo1');
 const pallo2 = document.getElementById('pallo2');
 const banner2 = document.getElementById("banner2");
 const banner3 = document.getElementById("banner3");
+const banner4 = document.getElementById("banner4");
+const banner5 = document.getElementById("banner5");
 const button = document.getElementById('scrollButton');
 const button2 = document.getElementById('scrollButton2');
 
@@ -53,14 +55,18 @@ function setBannerPage() {
             pallo1.src = 'pallofull.png';
             pallo2.src = 'palloTyhja.png';
             banner3.style.opacity = 0;
+            banner5.style.opacity = 0;
             banner2.style.opacity = 1;
+            banner4.style.opacity = 1;
             break;
         case 2:
             shoes.style.left = '-40.5vw';
             pallo1.src = 'palloTyhja.png';
             pallo2.src = 'pallofull.png';
             banner2.style.opacity = 0;
+            banner4.style.opacity = 0;
             banner3.style.opacity = 1;
+            banner5.style.opacity = 1;
             break;
         default:
             console.log('Invalid Page Number');
